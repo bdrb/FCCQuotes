7a84190ac6be84715b7359557932857e809e55dd
gennady tabala <gennady.tabala@gmail.com>
2016-07-05 18:50:19 +0300

    no message

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,20 @@
-$(document).ready(function () {    
+$(document).ready(function () {
+
+    function quoteReady(newQuote, quoteDiv) {
+     if(newQuote.quote.length > 0) {
+       quoteDiv.text(newQuote.quote);
+     }
+    }
+
+    var $quoteDiv = $("#res");  
+    
     $("#getQuote").on('click', function () {
-        $("#res1").text("newQuote.quote");
-        WikiquoteApi.openSearch("George Washington",
-            function (results) {                
-                $("msg").text(results[0]);
-                // Get quote
-                WikiquoteApi.getRandomQuote($("msg").text(),
+        WikiquoteApi.openSearch($("#query-title").val(), //"George Washington",
+            function (results) {
+                $("#author").text(results[0]);
+                WikiquoteApi.getRandomQuote(results[0],
                     function (newQuote) {
-                        $("#res").text(newQuote.quote);
+                        quoteReady(newQuote, $quoteDiv);
                     },
                     function (msg) {
                         alert(msg);
@@ -15,6 +22,7 @@ $(document).ready(function () {
                 );
             },
             function (msg) {
+                $("#author").text("error:" + results[0]);
                 alert(msg);
             }
         );
